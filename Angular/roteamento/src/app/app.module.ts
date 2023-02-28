f54e1fe08913be9e5148276fd9c1d33b2a3e5e63
MaianeSamara <maianesamara@gmail.com>
2023-02-28 17:33:57 -0300

    05 - Lazy-Loading

--- Angular/roteamento/src/app/app.module.ts
+++ Angular/roteamento/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app/app-routing.module';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
